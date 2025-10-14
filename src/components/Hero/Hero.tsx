9d07588a1a8b8a0a8aa123ae9e485b5ea3ad9bf8
admin <admin@MacBook-Air-admin.local>
2025-10-14 14:58:15 +0300

    feat(hero): make image configurable via props to support dynamic hero photos

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Hero.css';
 
-export default function Hero(){
+type heroProps={
+    img:string;
+}
+
+export default function Hero({img}:heroProps){
     return(
         <>
             <div className="hero">
 
-                <div className="hero_img">
+                <div className="hero_img" style={{backgroundImage:`url(${img}`}}>
                     <span className="hero_text">Elegance in simplicity, Earth’s harmony</span>
                     <button className="hero_button">new</button>
                 </div>
