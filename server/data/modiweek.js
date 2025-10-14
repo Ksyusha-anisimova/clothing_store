95acec72cb730e51e2cb338124c53d2f24693aa7
admin <admin@MacBook-Air-admin.local>
2025-10-14 14:58:15 +0300

    fix(modiweek): correct typo and align naming consistency

---
+++ server/data/modiweek.js
@@ -0,0 +1,24 @@
+const modiweek=[
+    {
+        "img": "/images/photo1.png",
+        "favorite": "/images/favorite.png",
+        "description": "Anywhere Dress",
+    },
+    {
+        "img": "/images/photo2.png",
+        "favorite": "/images/favorite.png",
+        "description": "Anywhere Dress",
+    },
+    {
+        "img": "/images/photo3.png",
+        "favorite": "/images/favorite.png",
+        "description": "Anywhere Dress",
+    },
+    {
+        "img": "/images/photo1.png",
+        "favorite": "/images/favorite.png",
+        "description": "Anywhere Dress",
+    },
+];
+
+module.exports = modiweek;
--- src/components/Modiweek/Modiweek.tsx
+++ src/components/Modiweek/Modiweek.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import Card, {CardProps} from '../Card/Card';
-
+import '/src/components/Modiweek/Modiweek.css';
 
 type ModiweekProps ={
     title:string;
 }
 
-export default function Modweek({title}:ModiweekProps){
+export default function Modiweek({title}:ModiweekProps){
   const [items, setItems] = React.useState<CardProps[]>([]);
   React.useEffect(()=>{
       async function loadData(){
-          const response = await fetch('/api/modweek');
-          const modweek = await response.json();
-          setItems(modweek);
+          const response = await fetch('/api/modiweek');
+          const modiweek = await response.json();
+          setItems(modiweek);
       }
       loadData();
   },[]);
 
   return(
       <>
-        <div className="modweek">
-            <span className="modweek_title">
+        <div className="modiweek">
+            <span className="modiweek_title">
                 {title}
             </span>
 
-            <div className="mode_gallery">
+            <div className="modiweek_gallery">
                 {items.map((item,index)=>(
                     <Card key={index} {...item}/>
                 ))}
